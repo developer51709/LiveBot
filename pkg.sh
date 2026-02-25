bd30e2a3934fc034239b58b95b9739d7fded4cce
SebOuellette <seb_ouellette@honeybeeks.net>
2026-02-24 22:07:32 -0500

    Support ARM releases to resolve #291

--- pkg.sh
+++ pkg.sh
@@ -2,10 +2,6 @@
 
 PACKAGE="npx @electron/packager"
 
-BIN_LIN="livebot-linux-x64"
-BIN_OSX="livebot-darwin-x64"
-BIN_WIN="livebot-windows-x64"
-
 # Make sure the patch is applied
 tar -xvf patch.tar
 
@@ -13,21 +9,19 @@ tar -xvf patch.tar
 mkdir build
 cd build
 
-$PACKAGE ../ --platform=darwin,linux,win32 --arch=x64 --icon=../resources/icons/logo
+$PACKAGE ../ --platform=darwin,linux,win32 --arch=x64,arm64 --icon=../resources/icons/logo
 
-tar -czvf "$BIN_LIN".tar.gz "$BIN_LIN" &
-tar -czvf "$BIN_OSX".tar.gz "$BIN_OSX" &
-zip -rv "$BIN_WIN".zip livebot-win32-x64
+# Archive
+for dir in $(ls); do
+	zip -rv "$dir".tar.gz "$dir" &
+done
+wait
 
-gpg -ab "$BIN_LIN".tar.gz
-gpg -ab "$BIN_OSX".tar.gz
-gpg -ab "$BIN_WIN".zip
-
-gpg --verify "$BIN_LIN".tar.gz.asc
-gpg --verify "$BIN_OSX".tar.gz.asc
-gpg --verify "$BIN_WIN".zip.asc
+# Sign
+for archive in $(ls *.gz); do
+	gpg -ab "$archive"
+	gpg --verify "$archive".asc
+done
 
 echo
 echo "Your releases have been created in $(pwd)"
-
-wait
