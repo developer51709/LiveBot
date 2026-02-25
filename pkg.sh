89998b14ac51455d849b1337af4b8a91fc487190
SebOuellette <seb_ouellette@honeybeeks.net>
2026-02-24 21:48:18 -0500

    Add pgp signing to the package script. Ready for next release.

--- pkg.sh
+++ pkg.sh
@@ -19,6 +19,14 @@ tar -czvf "$BIN_LIN".tar.gz "$BIN_LIN" &
 tar -czvf "$BIN_OSX".tar.gz "$BIN_OSX" &
 zip -rv "$BIN_WIN".zip livebot-win32-x64
 
+gpg -ab "$BIN_LIN".tar.gz
+gpg -ab "$BIN_OSX".tar.gz
+gpg -ab "$BIN_WIN".zip
+
+gpg --verify "$BIN_LIN".tar.gz.asc
+gpg --verify "$BIN_OSX".tar.gz.asc
+gpg --verify "$BIN_WIN".zip.asc
+
 echo
 echo "Your releases have been created in $(pwd)"
 
